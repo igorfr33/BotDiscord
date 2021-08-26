3c188b3b8c99713961b2ae54c659f21a3a97a67f
igorfr33 <igorrabelof10@gmail.com>
2021-08-26 15:17:01 -0300

    Integrando mensagem automatica

--- botCotacoes.py
+++ botCotacoes.py
@@ -1,5 +1,5 @@
-import discord
-from discord.ext import commands
+from discord import channel
+from discord.ext import commands, tasks
 import requests
 
 client = commands.Bot(command_prefix = ">", case_insensitive = True)
@@ -12,10 +12,21 @@ async def on_ready():
   async def ola(ctx):
     await ctx.send(f'Olá,{ctx.author} ')
 
-  @client.command()
-  async def PVU(ctx):
-    request = requests.get('https://api.coingecko.com/api/v3/coins/plant-vs-undead-token'.format())
-    pvu_data = request.json()
-    await ctx.send('Preco de Venda em Real: {}'.format(pvu_data['market_data']['current_price']['brl']))
+    @tasks.loop(minutes = 1)
+    async def cotacoes():
+      
+      requestPUV = requests.get('https://api.coingecko.com/api/v3/coins/plant-vs-undead-token'.format())
+      request2CYT = requests.get('https://api.coingecko.com/api/v3/coins/coinary-token'.format())
+      requestCCAR = requests.get('https://api.coingecko.com/api/v3/coins/cryptocars'.format())
 
-client.run('ODgwNDI4NzEzNTE3MzkxOTMz.YSeJIg.WVSeU9KfkI2CW3XVm3ekVf-wLHU')
+      pvu_data = requestPUV.json()
+      cyt_data = request2CYT.json()
+      ccar_data = requestCCAR.json()
+
+      canal = await client.get_channel(id)
+
+      await canal.send('PVU - R$: {}'.format(pvu_data['market_data']['current_price']['brl']))
+      await canal.send('CYT - R$: {}'.format(cyt_data['market_data']['current_price']['brl']))
+      await canal.send('CCAR - R$ {}'.format(ccar_data['market_data']['current_price']['brl']))
+
+      client.run('ODgwNDI4NzEzNTE3MzkxOTMz.YSeJIg.WVSeU9KfkI2CW3XVm3ekVf-wLHU')
