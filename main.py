f15ec48a7273b79a65d9faaee26594df6d80d9ca
igorfr33 <igorrabelof10@gmail.com>
2021-08-26 12:10:05 -0300

    Discord Bot Cotações CriptoMoedas

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+import discord
 import requests
+from discord.ext import commands
 
-request = requests.get('https://api.coingecko.com/api/v3/coins/plant-vs-undead-token'.format())
+client = commands.Bot(command_prefix = ">", case_insensitive = True)
 
-pvu_data = request.json()
+@client.event
+async def on_ready():
+  print('Entramos como {0.user}'.format(client))
 
-print('Preco de Venda em Real: {}'.format(pvu_data['market_data']['current_price']['brl']))
+  @client.command()
+  async def ola(ctx):
+    await ctx.send(f'Olá,{ctx.author} ')
+
+  @client.command()
+  async def PVU(ctx):
+    request = requests.get('https://api.coingecko.com/api/v3/coins/plant-vs-undead-token'.format())
+    pvu_data = request.json()
+    await ctx.send('Preco de Venda em Real: {}'.format(pvu_data['market_data']['current_price']['brl']))
+
+client.run('ODgwNDI4NzEzNTE3MzkxOTMz.YSeJIg.WVSeU9KfkI2CW3XVm3ekVf-wLHU')
